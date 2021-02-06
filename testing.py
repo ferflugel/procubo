e1d03d83aae733b9080de34e962e234cb0226e4e
Fernando Flugel Assad <fernandofassad@gmail.com>
2021-02-06 19:03:56 -0300

    commit

--- testing.py
+++ testing.py
@@ -1,1 +1,21 @@
-print("Testando GitHub");
+# Alguns exemplos simples 
+
+# 1 - print() 
+
+print("Oi") # 'Oi' sera mostrado
+
+# 2 - Variaveis 
+
+x = 1
+x = 3
+print(x) # '3' sera mostrado 
+
+# 3 - Caso mais complexo com variaveis
+
+x = 3
+y = 4
+x = y
+y = x
+print(x, y)
+
+
