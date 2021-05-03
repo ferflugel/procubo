6aaa3059d6afbfc5e16564729c51b4c8a4a8d689
Jhony <34777275+Jhouny@users.noreply.github.com>
2021-05-03 20:13:49 -0300

    V2 - modified folder paths to adapt to different folder structures. Changed terminal output to be 'fancier'

---
+++ YT_music_download/YT_Data/DownloadPlaylist.py
@@ -0,0 +1,63 @@
+from downloadFunctions import *
+import os, sys
+import DownloadVideo
+import DownloadAudio
+from time import sleep
+import pafy
+
+pafy.set_api_key("~API KEY here~")   #API key can be found at Google Cloud Platform,
+                        #after creating a project and enabling YouTube Data API v3
+
+def DownloadPlaylist():
+    url = "https://www.youtube.com/playlist?list=PLLcpBVEQD-CwpLaJzjFbKhBbAn2Wz4lle"
+
+    with open(os.path.join(dependencies.globalFolder, 'config.txt'), 'r') as f:
+        cont = f.read().split('\n')
+
+    location = os.path.abspath(cont[0])
+    t = str(cont[1])
+
+    #Initializing variables and retrieving playlist data
+    playlist = pafy.get_playlist2(url)
+    index = 0
+    state = True
+
+    print("\n" + "*" + "-"*80 + "*" + "\n" + " "*5 + "-"*72 +"\n")  #Aesthetic spacer for command outputs
+
+    while state:
+        if index > (len(playlist)-1):    #Check if there videos left to download
+            print("\tProcess finished successfully.\n")
+            print("\n" + " "*5 + "-"*72 + "\n" + "*" + "-"*80 + "*" + "\n")
+            state = False
+            continue
+        try:
+            if t == '1':        #Download as video
+                try:
+                    vid = playlist[index]
+                    print("\nCurrently in number %i and music name is: %s"%(index, vid.title))
+                    DownloadVideo.download(vid, location)
+
+                except Exception as e:
+                    print(e)
+                    sleep(0.5)
+
+
+            elif t == '0':    #Download as audio
+                try:
+                    aud = playlist[index]
+                    print("Currently in number %i and music name is: %s"%(index, aud.title))
+                    DownloadAudio.download(aud, location)
+
+                except Exception as e:
+                    print(e)
+                    sleep(0.5)
+
+        except Exception as e:
+            print("Process couldn't be concluded!\n", e)
+
+        index += 1    #Go to the next item in the playlist
+        print("\n"+"-"*80+"\n")    #Aesthetic spacer for command outputs
+
+if __name__ == "__main__":
+  if dependencies.proceed:
+      DownloadPlaylist()
---
+++ YT_music_download/YT_Data/downloadFunctions.py
@@ -0,0 +1,85 @@
+import dependencies
+import os
+from pydub import AudioSegment as audio
+from random import choice
+from mutagen.easyid3 import EasyID3
+from simple_image_download import simple_image_download as simp
+import shutil
+
+#Find if 'file' is in the 'path' provided
+def find(file, path):
+    contains = False
+    nm = ""
+    for root, dirs, files in os.walk(path):
+        for name in files:
+            if name == file or ".".join(name.split('.')[:-1]) == file:
+                contains = True
+                nm = name
+                break
+            else:
+                contains = False
+                nm = name
+    return contains, nm
+
+#Convert .webm file to .mp3
+def Cvt2Mp3(loc, f_name):
+    file_name = ".".join(f_name.split('.')[:-1]) if len(f_name.split('.')) > 1 else f_name
+    file_type = f_name.split('.')[-1]
+
+    file = os.path.join(loc, file_name)
+
+    aud_file = audio.from_file("%s"%(file+"."+file_type), format=file_type)          #Converting .webm file to .mp3
+    os.remove((file+"."+file_type))
+    aud_file.export("%s"%(file+".mp3"), format="mp3")
+
+def standardizeTitle(title):
+    forbidden = ['/', '?', '<', '>', "|", ',']
+    T = ""
+    for ind in range(len(title)):
+        if title[ind].lower() in forbidden:
+            T += "_"
+        else:
+            T += title[ind]
+
+    return T
+
+def folderTitlteStandardization(title):
+    forbidden = ['/', '?', '<', '>', ' ', ',', '|']
+    T = ""
+    for ind in range(len(title)):
+        if title[ind].lower() in forbidden:
+            T += "_"
+        else:
+            T += title[ind]
+
+    return T
+
+
+def GetMetadata(file, aud):
+    path = os.path.abspath(file)
+    pivotFile = os.path.join(dependencies.globalFolder, "pivotFile.mp3")  #"~~YOU MIGHT NEED TO CREATE A PIVOT FILE YOURSELF~~"
+    simpleImagesFolder = os.path.join(dependencies.globalFolder, "simple_images")
+    thumbnailFolder = os.path.join(simpleImagesFolder, folderTitlteStandardization(aud.title))    #"~~THE USED LIBRARY AUTOMATICALLY CREATES A FOLDER CALLED 'simple_images'"
+
+    response = simp.simple_image_download()
+    response.download(standardizeTitle(aud.title), 5)
+    shutil.move(path, pivotFile)
+
+    images = [i for g, h, i in os.walk(thumbnailFolder)]
+    thumbnail = os.path.join(thumbnailFolder, choice(os.listdir(thumbnailFolder)))
+
+    os.system("""ffmpeg -loglevel warning -i "%s" -i "%s" -map_metadata 0 -map 0:0 -map 1:0 -c copy -id3v2_version 3 -metadata:s:v comment="Cover (front)" "%s" """%(pivotFile, thumbnail, path))
+    # os.system("""ffmpeg -loglevel warning -i "%s" -i "%s" -map_metadata 0 -map 0 -map 1 "%s" """%(pivotFile, thumbnail, path))
+
+    audio = EasyID3(str(path))
+    audio['artist'] = aud.author
+    audio['title'] = aud.title
+
+    audio.save()
+
+    for f in os.listdir(thumbnailFolder):
+        if file.lower() != "thumbnail.jpg":
+            os.remove((os.path.join(thumbnailFolder, f)))
+    os.system("""rmdir "%s" """%thumbnailFolder)
+    os.system("""rmdir "%s" """%simpleImagesFolder)
+    os.remove(pivotFile)
