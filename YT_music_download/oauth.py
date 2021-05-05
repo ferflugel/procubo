932dadd61872397cebe5ffdc6c6cbf599b23a857
Fernando Flugel Assad <fernandofassad@gmail.com>
2021-05-04 23:00:08 -0300

    commit

--- YT_music_download/oauth.py
+++ YT_music_download/oauth.py
@@ -6,41 +6,45 @@ from google_auth_oauthlib.flow import InstalledAppFlow
 from google.auth.transport.requests import Request
 from googleapiclient.discovery import build
 
-credentials = None
+while True:
+    credentials = None
 
-if os.path.exists('token.pickle'):
-    print('Loading Credentials From File...')
-    with open('token.pickle', 'rb') as token:
-        credentials = pickle.load(token)
+    if os.path.exists('token.pickle'):
+        print('Loading Credentials From File...')
+        with open('token.pickle', 'rb') as token:
+            credentials = pickle.load(token)
 
-if not credentials or not credentials.valid:
-    if credentials and credentials.expired and credentials.refresh_token:
-        print('Refreshing Access Token...')
-        credentials.refresh(Request())
-    else:
-        print('Fetching New Tokens...')
-        flow = InstalledAppFlow.from_client_secrets_file(
-            '/Users/fernando/projeto/YT_music_download/client_secrets.json',
-            scopes=[
-                'https://www.googleapis.com/auth/youtube.readonly'
-            ]
-        )
+    if not credentials or not credentials.valid:
+        if credentials and credentials.expired and credentials.refresh_token:
+            print('Refreshing Access Token...')
+            credentials.refresh(Request())
+        else:
+            print('Fetching New Tokens...')
+            flow = InstalledAppFlow.from_client_secrets_file(
+                '/Users/fernando/projeto/YT_music_download/client_secrets.json',
+                scopes=[
+                    'https://www.googleapis.com/auth/youtube.readonly'
+                ]
+            )
 
-        flow.run_local_server(port=8080, prompt='consent',
-                              authorization_prompt_message='')
-        credentials = flow.credentials
+            flow.run_local_server(port=8080, prompt='consent',
+                                authorization_prompt_message='')
+            credentials = flow.credentials
 
-        # Save the credentials for the next run
-        with open('token.pickle', 'wb') as f:
-            print('Saving Credentials for Future Use...')
-            pickle.dump(credentials, f)
+            # Save the credentials for the next run
+            with open('token.pickle', 'wb') as f:
+                print('Saving Credentials for Future Use...')
+                pickle.dump(credentials, f)
 
 
-youtube = build("youtube", "v3", credentials = credentials)
-request = youtube.playlistItems().list(
-    part = "status", playlistId ="LLoYJm-CazxEVhojC9S6cH-g"
-)
+    youtube = build("youtube", "v3", credentials = credentials)
+    request = youtube.playlistItems().list(
+        part = "status", playlistId ="LLoYJm-CazxEVhojC9S6cH-g"
+    )
 
-response = request.execute()
+    response = request.execute()
 
-print(response)
+    with open('memory.txt', 'w') as f:
+        for video in response['items']:
+            f.write(video['id'])
+            f.write("\n")
