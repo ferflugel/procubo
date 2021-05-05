2f67f24bd679288f3890ca07258740b09384db06
Jhony <34777275+Jhouny@users.noreply.github.com>
2021-05-04 21:11:57 -0300

    some changes...

--- YT_music_download/YT_Data/dependencies.py
+++ YT_music_download/YT_Data/dependencies.py
@@ -10,6 +10,8 @@ except Exception as e:
     print("The following exception occurred:\n", e)
     proceed = False
 
+
+
 #Creating folder for data handling
 rootDir = os.getcwd().split('\\')[0]
 globalFolder = os.path.join(rootDir, "\\YT_Data")
@@ -18,13 +20,22 @@ if os.getcwd() != globalFolder:
     try:
         shutil.copytree(os.getcwd(), globalFolder)
         proceed = False
-        print("\n\tFinal destination folder created at '%s'"%globalFolder)
+        print("\n\tFinal destination folder created at '%s'\n"%globalFolder)
+        os.mkdir(os.path.join(globalFolder, "ffmpeg"))
+        ffmpeg = os.path.join(globalFolder, "ffmpeg.zip")
+        os.system("curl -L https://github.com/BtbN/FFmpeg-Builds/releases/download/autobuild-2021-05-04-12-33/ffmpeg-N-102349-ge27e80edcd-win64-gpl-shared.zip -o %s"%ffmpeg)
+        os.system("tar -xf %s -C %s"%(ffmpeg, os.path.join(globalFolder, "ffmpeg")))
     except FileExistsError:
         if len(os.listdir(globalFolder)) != len(os.listdir(os.getcwd())):
             shutil.rmtree(globalFolder)
             shutil.copytree(os.getcwd(), globalFolder)
             proceed = False
             print("\n\tFinal destination folder created at '%s'"%globalFolder)
+            os.mkdir(os.path.join(globalFolder, "ffmpeg"))
+            os.system("curl -L https://github.com/BtbN/FFmpeg-Builds/releases/download/autobuild-2021-05-04-12-33/ffmpeg-N-102349-ge27e80edcd-win64-gpl-shared.zip -o %s"%(os.path.join(globalFolder, "ffmpeg")))
+
+
+
 
 if not os.path.exists(os.path.join(globalFolder, 'config.txt')):
     destinationFolder = input("""\n\tWhere do you want the downloaded files to be stored? (In Windows, e.g. "C:\\Users\\~root~\\Music\\")\n\n\t\t>>> """)
