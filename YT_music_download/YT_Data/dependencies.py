c24d112e021cbc4894fdf904d1618273da92456e
Jhony <34777275+Jhouny@users.noreply.github.com>
2021-05-05 21:39:12 -0300

    ...

--- YT_music_download/YT_Data/dependencies.py
+++ YT_music_download/YT_Data/dependencies.py
@@ -1,8 +1,41 @@
-import os, sys, shutil
+import os, sys, shutil, logging, argparse
 from time import sleep
 
 libraries = ['pydub', 'youtube-dl', 'simple-image-download', 'mutagen', 'pafy']
 proceed=True
+
+
+#Configuring argparser for command line inputs
+parser = argparse.ArgumentParser()
+parser.add_argument(
+    "-log",
+    "--log",
+    default="warning",
+    help=(
+        "Provide logging level. "
+        "Example --log debug', default='warning'"),
+    )
+
+
+options = parser.parse_args()
+levels = {
+    'critical': logging.CRITICAL,
+    'error': logging.ERROR,
+    'warn': logging.WARNING,
+    'warning': logging.WARNING,
+    'info': logging.INFO,
+    'debug': logging.DEBUG
+}
+level = levels.get(options.log.lower())
+if level is None:
+    raise ValueError(
+        f"log level given: {options.log}"
+        f" -- must be one of: {' | '.join(levels.keys())}")
+
+logging.basicConfig(level=level)
+logger = logging.getLogger(__name__)
+
+
 
 try:
     os.system("pip install --upgrade -q -q %s"%(" ".join(libraries)))
