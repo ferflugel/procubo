c58fde33dac08f3d4fffd71301816fd6652ea441
Fernando Flugel Assad <63256562+ferflugel@users.noreply.github.com>
2021-05-31 21:30:13 -0300

    Update temp.py

--- DataVisualization/csv_reader/temp.py
+++ DataVisualization/csv_reader/temp.py
@@ -3,3 +3,8 @@ import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
 
+
+l1 = df['tip'].tolist()
+l2 = df['total_bill'].tolist()
+l3 = [pair for pair in zip(l1, l2)]
+l3.sort()
