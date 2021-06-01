a2142424544cad720e68d01851edd2665851dd96
felipebraz1 <69159036+felipebraz1@users.noreply.github.com>
2021-05-31 22:20:53 -0300

    Update temp.py

--- DataVisualization/csv_reader/temp.py
+++ DataVisualization/csv_reader/temp.py
@@ -8,3 +8,13 @@ l1 = df['tip'].tolist()
 l2 = df['total_bill'].tolist()
 l3 = [pair for pair in zip(l1, l2)]
 l3.sort()
+
+def spikes_counter(lst):
+  spikes = 0
+  for index in range(len(lst)):
+    m = (len[index+1][1] - len[index][1]) / (len[index+1][0] - len[index][0])
+    if abs(m) > 2:
+      spikes +=1
+  if spikes > 10:
+    return False
+  return True
