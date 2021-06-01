22c28f59d23d5c1ae4ff79104f10cf95b8f3e66c
Fernando Flugel Assad <63256562+ferflugel@users.noreply.github.com>
2021-05-31 22:38:22 -0300

    Update temp.py

--- DataVisualization/csv_reader/temp.py
+++ DataVisualization/csv_reader/temp.py
@@ -4,17 +4,24 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 
 
+df = sns.load_dataset("tips")
 l1 = df['tip'].tolist()
 l2 = df['total_bill'].tolist()
 l3 = [pair for pair in zip(l1, l2)]
 l3.sort()
 
 def spikes_counter(lst):
-  spikes = 0
-  for index in range(len(lst)):
-    m = (len[index+1][1] - len[index][1]) / (len[index+1][0] - len[index][0])
-    if abs(m) > 2:
-      spikes +=1
+    spikes = 0
+    slope = 0
+    for index in range(len(lst) - 1):
+        if (lst[index+1][0] - lst[index][0]) == 0:
+            slope = 1000
+        else:
+            slope = (lst[index+1][1] - lst[index][1]) / (lst[index+1][0] - lst[index][0])
+        if abs(slope) > 790:
+            print("SPIKE")
+            spikes += 1
     if spikes > 10:
-      return False
-  return True
+        print(spikes)
+        return False
+    return True
