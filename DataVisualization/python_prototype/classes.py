0608e4b9788713072179fd774e55fbebab55a966
Pedro H. Schisler Squisati <63691380+squisati@users.noreply.github.com>
2021-07-02 15:52:59 -0300

    Update classes.py

--- DataVisualization/python_prototype/classes.py
+++ DataVisualization/python_prototype/classes.py
@@ -23,10 +23,20 @@ class Plot:
         self.despine = input(f'Select plot despine:')
         
 class ColumnPlot(Plot):
-    pass
+    def __init__(self, plot_type, data, columns):
+        super().__init__(plot_type, data, columns)
+        self.width = 1
 
+    def select_specific_attributes(self):
+        self.width = input(f'Select column width: ')
+        
 class Histogram(Plot):
-    pass
+    def __init__(self, plot_type, data, columns):
+        super().__init__(plot_type, data, columns)
+        self.width = 1
+
+    def select_specific_attributes(self):
+        self.width = input(f'Select bin width: ')
 
 class PieChart(Plot):
     pass
@@ -35,7 +45,14 @@ class DonutChart(Plot):
     pass
 
 class ScatterPlot(Plot):
-    pass
+    def __init__(self, plot_type, data, columns):
+        super().__init__(plot_type, data, columns)
+        self.hue = None
+        self.best_fit = False
+    
+    def select_specific_attributes(self):
+        self.hue = input(f'Select hue: ')
+        self.best_fit = input(f'Show line of best fit: ')
 
 class LinePlot(Plot):
     def __init__(self, plot_type, data, columns):
